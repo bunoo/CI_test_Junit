fbbd0756f8033b733b94acbaef5dd025b7117c4b
abha <abha@example.com>
2019-06-03 09:51:44 +0530

    JUnit project for CI

---
+++ src/main/java/jUnitTestCases/UnitTestJUnit.java
@@ -0,0 +1,12 @@
+package jUnitTestCases;
+
+import org.junit.Test;
+
+public class UnitTestJUnit {
+
+	@Test
+	public void firstTestJUnit() {
+		System.out.println("My first test case in JUnit");
+	}
+	
+}
